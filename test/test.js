8b18eb698c6afb6fe159e501a61d02f8e3ae2b4a
Tat Uyen Tam <tattam192@gmail.com>
2024-09-25 23:19:45 +1000

    edit test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var expect = require("chai").expect;
 var request = require("request");
+var url = "http://localhost:3040";
 
 describe("Add Two Numbers", function () {
-    var url = "http://localhost:3040";
     it("Should return 8 if we request result for 3 + 5", function (done) {
         request(`${url}/addTwoNumber?n1=3&n2=5`, function (error, response, body) {
             let result = JSON.parse(response.body);
@@ -13,7 +13,6 @@ describe("Add Two Numbers", function () {
 });
 
 describe("Sub Two Numbers", function () {
-    var url = "http://localhost:3040";
     it("Should return 3 if we request result for 15 - 12", function (done) {
         request(`${url}/subTwoNumber?n1=15&n2=12`, function (error, response, body) {
             let result = JSON.parse(response.body);
